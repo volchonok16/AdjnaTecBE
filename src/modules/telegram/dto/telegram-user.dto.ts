6b16b1b3d928c9a4e7fdf569159aa8bb93e5ce7a
Stanislav <stanislaworonin@yandex.ru>
2023-10-07 12:19:03 +0300

    FIX: nullable lastName

--- src/modules/telegram/dto/telegram-user.dto.ts
+++ src/modules/telegram/dto/telegram-user.dto.ts
@@ -1,14 +1,14 @@
 export class TelegramUserDto {
   id: number;
-  firstName: string;
-  lastName: string;
+  firstName: string | null;
+  lastName: string | null;
   username: string;
 
   static create(data: any): TelegramUserDto {
     return {
       id: data.id,
-      firstName: data.first_name,
-      lastName: data.last_name,
+      firstName: data.first_name ?? null,
+      lastName: data.last_name ?? null,
       username: data.username,
     };
   }
